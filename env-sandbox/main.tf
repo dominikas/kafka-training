4d291dfd96ffa19f24f7b0a6c9f569aa00227942
Dominika Saide <dominika.saide@stepstone.com>
2024-02-02 15:28:01 +0100

    initial network release

---
+++ env-sandbox/main.tf
@@ -0,0 +1,17 @@
+# Network configuration
+module "aws-network" {
+  source = "github.com/dominikas/kafka-training/tree/main/module-aws-network"
+  env_name = local.env_name
+  vpc_name = "msur-VPC"
+  cluster_name = local.k8s_cluster_name
+  aws_region = local.aws_region
+  main_vpc_cidr = "10.10.0.0/16"
+  public_subnet_a_cidr = "10.10.0.0/18"
+  public_subnet_b_cidr = "10.10.64.0/18"
+  private_subnet_a_cidr = "10.10.128.0/18"
+  private_subnet_b_cidr = "10.10.192.0/18"
+}
+
+# EKS Config
+
+# GitOps Config
